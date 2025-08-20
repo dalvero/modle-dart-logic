a3e0f8549d8f993a2ce1cf2544dbd500c2ee72fa
Daniel Alvero <dalvero589@gmail.com>
2025-08-20 13:20:37 +0700

    first init

---
+++ bin/modle_dart_logic.dart
@@ -0,0 +1,5 @@
+import 'package:modle_dart_logic/modle_dart_logic.dart' as modle_dart_logic;
+
+void main(List<String> arguments) {
+  print('Hello world: ${modle_dart_logic.calculate()}!');
+}
---
+++ test/modle_dart_logic_test.dart
@@ -0,0 +1,8 @@
+import 'package:modle_dart_logic/modle_dart_logic.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
